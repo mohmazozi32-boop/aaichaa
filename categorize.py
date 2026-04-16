37f1e66b030378dc1522caafe5ec11a1be000318
mohmazozi32-boop <mohmazozi32@gmail.com>
2026-04-16 22:42:28 +0100

    Delete categorize.py

--- categorize.py
+++
@@ -1,7 +0,0 @@
-class Categorizer:
-    def classify_commune(self, commune):
-        """
-        تصنيف البلدية حسب المنطقة الحرارية الشتوية
-        إذا لم توجد قيمة، يرجع 'Inconnue'
-        """
-        return commune.get("thermal_zone_winter", "Inconnue")
